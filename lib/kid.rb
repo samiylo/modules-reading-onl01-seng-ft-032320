1ddce9885be923b01434dfe271d6e79cb11bd8bb
Samiylo Kryshu <skryshu@gmail.com>
2020-04-07 22:43:54 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -2,7 +2,7 @@ require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 require_relative './fancy_dance.rb'
 class Kid 
-  include Dance 
+  
   
   attr_accessor :name
  
